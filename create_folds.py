ee906de7742e95106c353d32c33890d8abd84a38
derek-byte <derek.s.prog@gmail.com>
2022-03-07 20:32:11 -0500

    Installed Tensorflow and Numpy

--- create_folds.py
+++ create_folds.py
@@ -2,6 +2,6 @@ import os
 import pandas as pd
 from sklearn import model_selection 
 
-if __name__ == '__main__':
-    input_path = 
-    df.pd.read_csv(os.path.join(input_path, "train.csv"))
+# if __name__ == '__main__':
+#     input_path = 
+#     df.pd.read_csv(os.path.join(input_path, "train.csv"))
