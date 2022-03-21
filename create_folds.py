e4f63e2b545dc04d9c9b3fdd064815882de24747
Derek Sheen <derek.s.prog@gmail.com>
2022-03-21 19:59:27 -0400

    Delete create_folds.py

--- create_folds.py
+++
@@ -1,7 +0,0 @@
-import os
-import pandas as pd
-from sklearn import model_selection 
-
-# if __name__ == '__main__':
-#     input_path = 
-#     df.pd.read_csv(os.path.join(input_path, "train.csv"))
